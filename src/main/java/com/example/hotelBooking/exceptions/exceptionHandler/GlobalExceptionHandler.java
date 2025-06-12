03465598805407b4e9de651b98ce5f65f886f991
agiri <amit.giri@here.com>
2025-06-12 19:52:17 +0530

    added more business logic along with JUnit test cases

--- src/main/java/com/example/hotelBooking/exceptions/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/hotelBooking/exceptions/exceptionHandler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.hotelBooking.exceptions.exceptionHandler;
 
+import com.example.hotelBooking.exceptions.BookingNotFoundException;
 import com.example.hotelBooking.exceptions.RoomNotAvailableException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,16 @@ public class GlobalExceptionHandler
 {
   @ExceptionHandler(RoomNotAvailableException.class)
   public ResponseEntity<Map<Object, Object>> handleRoomNotAvailableException(Exception e, WebRequest request){
+    Map<Object, Object> response = new HashMap<>();
+    response.put("timestamp", Instant.now());
+    response.put("message", e.getMessage());
+    response.put("description", request.getDescription(false));
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+  }
+
+  @ExceptionHandler(BookingNotFoundException.class)
+  public ResponseEntity<Map<Object, Object>> handleBookingNotFoundException(Exception e, WebRequest request){
     Map<Object, Object> response = new HashMap<>();
     response.put("timestamp", Instant.now());
     response.put("message", e.getMessage());
